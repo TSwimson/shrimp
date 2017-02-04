ffd2078ba555b9aae1e10a4d85cf271514e46a88
Tanner Swenson <tanner@jetinsight.com>
2017-02-04 00:03:56 -0800

    remove auto kill from require file

--- lib/shrimp.rb
+++ lib/shrimp.rb
@@ -4,12 +4,3 @@ require 'shrimp/server_configuration'
 require 'shrimp/render_configuration'
 require 'shrimp/rasterize_server'
 require 'shrimp/rasterize_client'
-
-at_exit do
-  s = Shrimp::RasterizeServer.new
-  s.send(:read_pid_from_file)
-  if s.running?
-    puts 'killing'
-    s.stop
-  end
-end
